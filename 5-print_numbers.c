c776a2f4bc30c5020f9df9c0c2f0c0d0fdb73b36
KhalilFadiga1 <khalilfadiga667@gmail.com>
2023-02-17 21:52:38 -0800

    print single digits of base 10

---
+++ 5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that print all single digits of base 10
+ * Return - 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	for (i = 0; i < 10;i++)
+	{
+		printf("%d \n",i);
+	}
+}
