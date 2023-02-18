dfa9188f2d2fc2f4eaab540baa4e451c99210867
KhalilFadiga1 <khalilfadiga667@gmail.com>
2023-02-17 22:02:22 -0800

    6-print_numberz.c

---
+++ 6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints single digits of base 10
+ * Return - 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; i < 10;i++)
+	{
+		putchar(i);
+	}
+}
+
