f84a25ab52dd35669f28a6c50ff414e15543b558
KhalilFadiga1 <khalilfadiga667@gmail.com>
2023-06-15 22:52:43 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line with puts function
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the sizes of various types on a computer
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	printf
