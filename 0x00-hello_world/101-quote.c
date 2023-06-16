6d8e69a45742d2cba68813f5124ed6b311b19ec9
KhalilFadiga1 <khalilfadiga667@gmail.com>
2023-06-15 23:37:27 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A program that prints a line with fputs to a standard error
- * Return: (1);
+ * Return: (1)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
-}   
+}
