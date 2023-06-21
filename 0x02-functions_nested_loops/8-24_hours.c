0f19fdbbaafdc469f95921ac7912af85c333ebec
KhalilFadiga1 <khalilfadiga667@gmail.com>
2023-06-21 01:01:33 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - Print every minute of the day of Jack bauer, starting from 00:00 to 23:59
+ * jack_bauer - Print every minute of the day of Jack bauer
  * Return: Nothing
  */
 void jack_bauer(void)
