f59296c4078f64f0c1359a110980b9a067017ddc
KhalilFadiga1 <khalilfadiga667@gmail.com>
2023-06-21 00:44:39 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,14 @@
 
 /**
  * _isalpha - Check for alphabetic characters
- * @c - The character to be checked
+ * @c: The character to be checked
  * Return: 1 for character checked and 0 for any other results
  */
 int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
 	{
-	       	return (1);
+	return (1);
 	}
 	return (0);
-}	
+}
