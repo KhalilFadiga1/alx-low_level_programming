9dcb49162433e052ada8f8fce4b879222364dfff
KhalilFadiga1 <khalilfadiga667@gmail.com>
2023-06-22 11:08:11 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,19 +8,19 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-	if ((i % 3) == 0)
+	if (i % 15 == 0)
+	{
+	printf("FizzBuzz");
+	}
+	else if (i % 3 == 0)
 	{
 	printf("Fizz");
 	}
-	else if ((i % 5) == 0)
+	else if (i % 5 == 0)
 	{
 	printf("Buzz");
-	}
-	else if ((i % 15) == 0)
-	{
-	printf("FizzBuzz");
 	}
 	else
 	{
