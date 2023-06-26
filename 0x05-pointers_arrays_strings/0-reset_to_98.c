85507668e13e9e6da9c1454a13dc9add65345caa
KhalilFadiga1 <khalilfadiga667@gmail.com>
2023-06-26 01:02:11 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset a pointer n to value 98
+ * @n: Th pointer whose value to be reset
+ * Return: Nothing
+ */
+int reset_to_98(int *n)
+{
+	*n = 98;
+	_putchar('\n');
+}
