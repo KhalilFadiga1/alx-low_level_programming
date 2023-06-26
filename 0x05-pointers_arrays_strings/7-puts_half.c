bcacd7b197c6088a2d9c6bf5221c4d812f625c3e
KhalilFadiga1 <khalilfadiga667@gmail.com>
2023-06-26 10:12:06 -0700

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,14 +3,13 @@
 /**
  * puts_half - a function that prints a half string,followed
  * by new line
- * @str - string to be reversed
+ * @str: string to be reversed
  */
 void puts_half(char *str)
 {
 	int j = 0;
 	int k;
 
-	
 	while (str[j] != '\0')
 	{
 	j++;
