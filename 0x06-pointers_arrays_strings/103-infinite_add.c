63ab0e2312e31b471ddde199847a42170ee68e45
KhalilFadiga1 <khalilfadiga667@gmail.com>
2023-06-30 22:24:34 -0700

    103-add

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ * res_string - reserve array
+ * @n: Input value
+ */
+void res_string(char *n)
+{
+	int i = 0, j = 0;
+	char temp;
+
+	while (!(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+	for (j = 0; j < i; i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * infinite_add - function that add two numbers
+ * @n1: Input value
+ * @n2: Input value
+ * @r: Total of the addition
+ * @size_r: Input value
+ * Return: r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
+
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	res_string(r);
+	return (r);
+}
