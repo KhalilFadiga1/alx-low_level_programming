d6fa4229eeff44178281727515b82dff148b4a5c
KhalilFadiga1 <khalilfadiga667@gmail.com>
2023-07-03 03:55:13 -0700

    7-print_chessboard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/***
+ * _strstr - function that locates a substring
+ * @haystack: Input value
+ * @needle: Input value
+ * Return: 0 (On Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+}
