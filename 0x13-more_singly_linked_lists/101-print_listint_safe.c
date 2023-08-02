43e7150a30aff9db6695be66238906c1b093cbad
KhalilFadiga1 <khalilfadiga667@gmail.com>
2023-08-02 12:07:31 -0700

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -64,12 +64,12 @@ size_t print_listint_safe(const listint_t *head)
 	}
 	else
 	{
-		for (; index < nodes; index++)
+		for (index = 0; index < nodes; index++)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
 		}
-		printf("->[%p] %d\n", (void *)head, head->n);
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 	return (nodes);
 }
