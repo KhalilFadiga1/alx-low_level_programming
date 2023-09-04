3772a952364ddfbc410d241b9c3ab56dc21c2b0d
KhalilFadiga1 <khalilfadiga667@gmail.c>
2023-09-04 08:58:41 -0700

    copy

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+void close_file(int ab);
+char *create_buffer(char *file);
+
+/**
+ * create_buffer - Allocation of 1024 bytes to a buffer
+ * @file: Buffer name to store the characters
+ * Return: An address to newly-allocated buffer
+ */
+char *create_buffer(char *file)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (buf);
+}
+
+/**
+ * close_file - Endd the file descriptors
+ * @ab: Closed file descriptor
+ */
+void close_file(int ab)
+{
+	int z;
+
+	z = close(ab);
+	if (z == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close ab %d\n", ab);
+		exit(100);
+	}
+}
+
+/**
+ * main - Duplicates the contents of a file to different file
+ * @argc: Argument counter
+ * @argv: Argument vector
+ * Return: 0 (Always Success)
+ */
+int main(int argc, char *argv[])
+{
+	int frm, too, m, n;
+	char *buf;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: dp file_from file_to\n");
+		exit(97);
+	}
+	buf = create_buffer(argv[2]);
+	frm = open(argv[1], O_RDONLY);
+	m = read(frm, buf, 1024);
+	too = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	do {
+		if (frm == -1 || m == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+			free(buf);
+			exit(98);
+		}
+		n = write(too, buf, m);
+		if (too == -1 || n == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buf);
+			exit(99);
+		}
+		m = read(frm, buf, 1024);
+		too = open(argv[2], O_WRONLY | O_APPEND);
+	} while (m > 0);
+	free(buf);
+	close_file(frm);
+	close_file(too);
+	return (0);
+}
