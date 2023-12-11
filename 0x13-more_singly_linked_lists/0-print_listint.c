31c332702ab375fae1c9eab75b9d4c3b884c7b55
KhalilFadiga1 <khalilfadiga667@gmail.com>
2023-12-11 04:33:25 -0800

    More projects on Singly Linked List

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - Prints All Elements of Listint_t list
+ * @h: Ptr to Head of the Listint_t list
+ * Return: Total figure of the nodes in the listint_t list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nds = 0;
+	
+	while (h)
+	{
+		nds++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nds);
+}
