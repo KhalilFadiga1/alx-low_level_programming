220b3016dee4c9fed0177dd78c093553ad0096ee
KhalilFadiga1 <khalilfadiga667@gmail.com>
2024-01-29 11:48:38 -0800

    Update

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c *.c -fpic && gcc *.o -shared -o liball.so
+gcc -Wall -Wextra -Werror -pedantic -c -L. *.c -fpic && gcc *.o -shared -o liball.so
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
