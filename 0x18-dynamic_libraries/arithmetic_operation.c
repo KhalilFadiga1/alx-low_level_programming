7cfbbab3f70982223830809426fe60cde836d8f0
KhalilFadiga1 <khalilfadiga667@gmail.com>
2024-01-30 00:59:40 -0800

    Shared file created

---
+++ 0x18-dynamic_libraries/arithmetic_operation.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - Add two numbers
+ * @x: First integer
+ * @y: Second Integer
+ * Return: Sum of the integers
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+/**
+ * sub - Find the diff between two numbers
+ * @x: First integer
+ * @y: Second Integer
+ * Return: Difference of the integers
+ */
+int sub(int x, int y)
+{
+        return (x - y);
+}
+
+/**
+ * mul - Product of two numbers
+ * @x: First integer
+ * @y: Second Integer
+ * Return: Product of the integers
+ */
+int mul(int x, int y)
+{
+        return (x * y);
+}
+
+/**
+ * div - Divide two numbers
+ * @x: First integer
+ * @y: Second Integer
+ * Return: Result of the division
+ */
+int div(int x, int y)
+{
+        return (x / y);
+}
+
+/**
+ * mod - Find modulo of two numbers
+ * @x: First integer
+ * @y: Second Integer
+ * Return: remainder of the modulo
+ */
+int mod(int x, int y)
+{       
+        return (x % y);
+}       
